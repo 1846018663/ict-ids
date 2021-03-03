0fb9c96409cedb6e4c22f26c66eb15b7507cee1d
leichengrong <1846018663@qq.com>
2021-03-03 09:02:21 +0800

    订单实体类提交

---
+++ src/main/java/com/hnu/ict/ids/entity/OrderInfo.java
@@ -0,0 +1,167 @@
+package com.hnu.ict.ids.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+
+@Data
+@TableName(value = "order_info")
+public class OrderInfo {
+    /** 主键ID **/
+    @TableId(type = IdType.AUTO)
+    private BigInteger id;
+
+
+    /**
+     * 订单来源
+     */
+    private String orderSurce;
+
+
+    /**
+     * 原始订单id 如乘客服务系统订单id
+     */
+    private String sourceOrderId;
+
+
+    /**
+     * 本系统订单号
+     */
+    private String orderNo;
+
+
+    /**
+     * 出发站id
+     */
+    private int beginStationId;
+
+
+    /**
+     * 终点站id
+     */
+    private int endStationId;
+
+
+    /**
+     * 车票数量
+     */
+    private int ticketNumber;
+
+
+    /**
+     * 购票用户id
+     */
+    private BigInteger buyUid;
+
+
+    /**
+     *开始时间
+     */
+    private Date startTime;
+
+
+    /**
+     * 行程id
+     */
+    private BigInteger travelId;
+
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+
+    public BigInteger getId() {
+        return id;
+    }
+
+    public void setId(BigInteger id) {
+        this.id = id;
+    }
+
+    public String getOrderSurce() {
+        return orderSurce;
+    }
+
+    public void setOrderSurce(String orderSurce) {
+        this.orderSurce = orderSurce;
+    }
+
+    public String getSourceOrderId() {
+        return sourceOrderId;
+    }
+
+    public void setSourceOrderId(String sourceOrderId) {
+        this.sourceOrderId = sourceOrderId;
+    }
+
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    public int getBeginStationId() {
+        return beginStationId;
+    }
+
+    public void setBeginStationId(int beginStationId) {
+        this.beginStationId = beginStationId;
+    }
+
+    public int getEndStationId() {
+        return endStationId;
+    }
+
+    public void setEndStationId(int endStationId) {
+        this.endStationId = endStationId;
+    }
+
+    public int getTicketNumber() {
+        return ticketNumber;
+    }
+
+    public void setTicketNumber(int ticketNumber) {
+        this.ticketNumber = ticketNumber;
+    }
+
+    public BigInteger getBuyUid() {
+        return buyUid;
+    }
+
+    public void setBuyUid(BigInteger buyUid) {
+        this.buyUid = buyUid;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public BigInteger getTravelId() {
+        return travelId;
+    }
+
+    public void setTravelId(BigInteger travelId) {
+        this.travelId = travelId;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
